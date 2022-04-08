7e6a6a615d0e295a9589f81401fc1b2166b1b219
Hunter Gregory <hunterlgregory@gmail.com>
2022-04-08 11:24:17 -0700

    generate scale yamls

---
+++ scale-testing/generate-yamls.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+numDeployments=10
+numNetpolsPerDeployment=2
+
+echo "creating $numDeployments deployments and $numNetpolsPerDeployment netpols per deployment"
+
+mkdir -p deployments
+mkdir -p netpols
+rm deployments/deployment*.yaml
+rm netpols/netpol*.yaml
+
+for (( i=1; i<=$numDeployments; i++ )); do
+    ## generate deployment yaml
+    toFile=deployments/deployment-$i.yaml
+    sed "s/nameReplace/test-deployment-$i/g" example-deployment.yaml > $toFile
+    sed -i "s/nsReplace/test-ns-$i/g" $toFile
+    sed -i "s/labelReplace/label-$i/g" $toFile
+
+    ## generate all netpols for this deployment
+    for (( j=1; j<=$numNetpolsPerDeployment; j++ )); do
+        toFile=netpols/netpol-$i-$j.yaml
+        sed "s/nameReplace/test-netpol-$i-$j/g" example-netpol.yaml > $toFile
+        sed -i "s/nsReplace/test-ns-$i/g" $toFile
+        z=$(( i + (j-1)*3 ))
+        plus1=$(( z % numDeployments + 1))
+        sed -i "s/labelReplace1/label-$plus1/g" $toFile
+        plus2=$(( (z+1) % numDeployments + 1))
+        sed -i "s/labelReplace2/label-$plus2/g" $toFile
+        plus3=$(( (z+2) % numDeployments + 1))
+        sed -i "s/labelReplace3/label-$plus3/g" $toFile
+    done
+done
+
+echo "done"
