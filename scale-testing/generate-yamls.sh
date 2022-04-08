082b59009fc4e632f7038882292ad2ae98faa607
Hunter Gregory <hunterlgregory@gmail.com>
2022-04-08 13:15:59 -0700

    remove namespace in yamls

--- scale-testing/generate-yamls.sh
+++ scale-testing/generate-yamls.sh
@@ -13,14 +13,12 @@ for (( i=1; i<=$numDeployments; i++ )); do
     ## generate deployment yaml
     toFile=deployments/deployment-$i.yaml
     sed "s/nameReplace/test-deployment-$i/g" example-deployment.yaml > $toFile
-    sed -i "s/nsReplace/test-ns-$i/g" $toFile
     sed -i "s/labelReplace/label-$i/g" $toFile
 
     ## generate all netpols for this deployment
     for (( j=1; j<=$numNetpolsPerDeployment; j++ )); do
         toFile=netpols/netpol-$i-$j.yaml
         sed "s/nameReplace/test-netpol-$i-$j/g" example-netpol.yaml > $toFile
-        sed -i "s/nsReplace/test-ns-$i/g" $toFile
         sed -i "s/labelReplace1/label-$i/g" $toFile
         z=$(( i + (j-1)*2 ))
         plus1=$(( z % numDeployments + 1))
