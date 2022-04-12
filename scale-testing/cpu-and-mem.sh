a19e1bd4e2a0f996f946162bf075fc97948d9c90
Hunter Gregory <hunterlgregory@gmail.com>
2022-04-12 11:20:21 -0700

    csv file extension for cpu and mem

--- scale-testing/cpu-and-mem.sh
+++ scale-testing/cpu-and-mem.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 sleepSeconds=65
 
-echo "time,pod,cpu,mem" > cpu-and-mem-pod-results.txt
-echo "time,node,cpu,cpuPercent,mem,memPercent" > cpu-and-mem-node-results.txt
+echo "time,pod,cpu,mem" > cpu-and-mem-pod-results.csv
+echo "time,node,cpu,cpuPercent,mem,memPercent" > cpu-and-mem-node-results.csv
 while true; do
     currentTime=`date -u`
     echo "running k top pod"
     lines=`kubectl top pod -n kube-system | grep npm | awk '{$1=$1;print}' | tr ' ' ',' | tr -d 'm' | tr -d 'Mi'`
     for line in $lines; do
-        echo "$currentTime,$line" >> cpu-and-mem-pod-results.txt
+        echo "$currentTime,$line" >> cpu-and-mem-pod-results.csv
     done
 
     currentTime=`date -u`
     echo "running k top node"
     lines=`kubectl top node | grep -v NAME | awk '{$1=$1;print}' | tr ' ' ',' | tr -d 'm' | tr -d 'Mi' | tr -d '%'`
     for line in $lines; do
-        echo "$currentTime,$line" >> cpu-and-mem-node-results.txt
+        echo "$currentTime,$line" >> cpu-and-mem-node-results.csv
     done
     echo "sleeping $sleepSeconds seconds"
     sleep $sleepSeconds
