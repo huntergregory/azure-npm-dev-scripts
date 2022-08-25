55a26c632ba38528a46c865f716f40c5e0b8ea91
Hunter Gregory <hunterlgregory@gmail.com>
2022-08-25 14:11:13 -0700

    fix log collection

--- chaos-jr/collect-all-npm-logs.sh
+++ chaos-jr/collect-all-npm-logs.sh
@@ -3,9 +3,10 @@ if [[ $1 == "" ]]; then
   exit 1
 fi
 
+set -x
 npmPods=`kubectl get pod -n kube-system | grep npm | awk '{print $1}'`
 for pod in $npmPods; do
-    logFilePath="$pod.log"
+    logFilePath="$1/$pod.log"
     echo "Saving npm logs to $logFilePath in a background process"
-    kubectl --kubeconfig=$1 logs -n kube-system $npmPod -f > $logFilePath &
+    kubectl logs -n kube-system -f $pod --kubeconfig=$1 > $logFilePath &
 done
