53d9a1bb2deaadd33186523b70850c292e4103ef
Hunter Gregory <hunterlgregory@gmail.com>
2022-08-25 13:58:00 -0700

    collect npm logs

---
+++ chaos-jr/collect-all-npm-logs.sh
@@ -0,0 +1,11 @@
+if [[ $1 == "" ]]; then
+  echo "specify the kube config file as first arg"
+  exit 1
+fi
+
+npmPods=`kubectl get pod -n kube-system | grep npm | awk '{print $1}'`
+for pod in $npmPods; do
+    logFilePath="$localResultsFolderName/$logFileName"
+    echo "Saving npm logs to $logFilePath in a background process"
+    kubectl --kubeconfig=$1 logs -n kube-system $npmPod -f > $logFilePath &
+done
