6e5e821271f884dc88c303af10f07cdccfc00931
Hunter Gregory <hunterlgregory@gmail.com>
2022-10-20 14:32:27 -0700

    update cluster creation scripts

--- windows-demo/aks-cluster-with-managed-npm.sh
+++ windows-demo/aks-cluster-with-managed-npm.sh
@@ -1,3 +1,5 @@
+# https://msazure.visualstudio.com/CloudNativeCompute/_wiki/wikis/CloudNativeCompute.wiki/225209/Manual-Test-AKS-WS2022-Cluster?anchor=method-2---aks-supports-ws2022-by-%60%60%60aks-custom-headers-ossku%60%60%60
+
 set -e
 
 myLocation="eastus2euap" # Depends on you
@@ -7,13 +9,17 @@ myWindowsUserName="azureuser" # Recommend azureuser
 myWindowsPassword="TODO" # Complex enough
 myWindowsNodePool="win22" # Length <= 6
 
+echo "creating cluster $myAKSCluster at $(date) in region $myLocation and RG $myResourceGroup"
+
 # Update aks-preview to the latest version
 az extension add --name aks-preview
 az extension update --name aks-preview
 
+az feature register --namespace Microsoft.ContainerService --name WindowsNetworkPolicyPreview
+az provider register -n Microsoft.ContainerService
+
 az group create --name $myResourceGroup --location $myLocation
 
-# for public preview and general availability, won't need to apply the yamls below and can just add the following to the create command: --network-policy azure
 az aks create \
     --resource-group $myResourceGroup \
     --name $myAKSCluster \
@@ -21,7 +27,11 @@ az aks create \
     --windows-admin-username $myWindowsUserName \
     --windows-admin-password $myWindowsPassword \
     --network-plugin azure \
-    --node-count 1
+    --network-policy azure \
+    --node-vm-size "Standard_DS2_v2" \
+    --node-count 1 \
+    --max-pods 100 \
+    --uptime-sla
 
 az aks nodepool add \
     --resource-group $myResourceGroup \
@@ -29,15 +39,11 @@ az aks nodepool add \
     --name $myWindowsNodePool \
     --os-type Windows \
     --os-sku Windows2022 \
-    --node-count 1
+    --node-vm-size Standard_D4s_v3 \
+    --node-count 1 \
+    --max-pods 100
 
 # uncomment below line to force pods to be scheduled on windows nodes
 # az aks nodepool update --node-taints CriticalAddonsOnly=true:NoSchedule -n nodepool1 -g $myResourceGroup --cluster-name $myAKSCluster
 
-# will set current kubectl context
 az aks get-credentials -g $myResourceGroup -n $myAKSCluster --overwrite-existing
-
-# NPM on Linux
-kubectl apply -f https://raw.githubusercontent.com/Azure/azure-container-networking/master/npm/azure-npm.yaml
-# NPM on Windows
-kubectl apply -f https://raw.githubusercontent.com/Azure/azure-container-networking/master/npm/examples/windows/azure-npm.yaml
